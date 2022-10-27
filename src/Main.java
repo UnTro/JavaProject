92133c05dfac437a621d52e2b43859495c57daaf
Ivan <untro999@yandex.ru>
2022-10-27 16:39:07 +0300

    First and last

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+        boolean a = true;
+        System.out.println(a);
+        Boolean b = true;
+        System.out.println(b.toString());
+
+        int i = 2147483647;
+        System.out.println(i+1);
+        Integer j = 2147483647;
+        System.out.println(j);
+    }
+}
